260a5a9a7ae50aabbceef5c371dff69edce0c784
Harish-Devpt <maddalharish@gmail.com>
2025-11-02 16:40:02 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { base } from 'framer-motion/client'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
      tailwindcss(),
-    
   ],
+  base: process.env.VITE_BASE_PATH || "/Colossal-Lab_UpGrade"
 })
